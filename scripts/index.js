9460586bee4078a5383b920eac712b9fb6799bbc
MatheusMFeitosa <matheusmf.feitosa@gmail.com>
2026-08-19 18:27:06 -0300

    Função que garda  conteudo dos produtos no localStorage para futuramente serem adicionados ao carrinho.

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,12 @@ botaoCarrinho.addEventListener("click", ()=>{
 })
 
 function adicionarCarrinho (nome, preco){
-    localStorage.setItem(nomeProduto, nome)
-    localStorage.setItem(precoProduto, preco)    
 
-    console.log(localStorage.getItem(nomeProduto), localStorage.getItem(precoProduto))
+    // utilizando um dicionario, para guardar os valores (nome, preco) e utilizando do metodo do JASON.stringify 
+    // para transformar em String e guardando na variavel nome para salvar em objetos diferentes
+    let lista = {"nomeProduto": nome, "precoProduto": preco}
+    localStorage.setItem(nome, JSON.stringify(lista))
 }
 
+
+// function verifica
