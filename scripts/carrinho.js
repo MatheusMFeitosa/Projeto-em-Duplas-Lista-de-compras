f57acc17b7673648de159e961f4f7527b8d58754
MatheusMFeitosa <matheusmf.feitosa@gmail.com>
2026-08-21 12:45:34 -0300

    Funções para inicializar todos os produtos e adicionar quantidade ao produto expecifico. Além de gerenciamento de filtros.

--- scripts/carrinho.js
+++ scripts/carrinho.js
@@ -2,6 +2,34 @@ const botaoInicio = document.querySelector(".fa-house");
 
 console.log(botaoInicio)
 
-botaoInicio.addEventListener("click", ()=>{
+botaoInicio.addEventListener("click", () => {
     window.location.href = "index.html"
 })
+
+function deletarItem(nomeProduto) {
+    let valor = localStorage.getItem(nomeProduto)
+    let listaOriginal = JSON.parse(valor)
+
+    listaOriginal["quantidade"] = 0
+
+    localStorage.setItem(listaOriginal["nomeProduto"], JSON.stringify(listaOriginal))
+
+    mostrarProdutosTela()
+}
+
+function mostrarProdutosTela(){
+    for (let i = 0; i < localStorage.length; i++) {
+        let listaOriginal = JSON.parse(localStorage.getItem(localStorage.key(i)))
+
+        
+        if(listaOriginal["quantidade"] > 0){
+            
+            console.log(listaOriginal["nomeProduto"])
+
+        }
+        
+    }
+}
+
+
+mostrarProdutosTela()
--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,65 @@
+const LISTA_PRODUTOS = [
+    { nomeProduto: "Tomate", precoProduto: 3.50, categoria: "alimentos", quantidade: 0 },
+    { nomeProduto: "Feijao", precoProduto: 10.00, categoria: "alimentos", quantidade: 0 },
+    { nomeProduto: "Farinha", precoProduto: 5.00, categoria: "alimentos", quantidade: 0 },
+    { nomeProduto: "Arroz", precoProduto: 5.00, categoria: "alimentos", quantidade: 0 }
+];
+
 const botaoCarrinho = document.querySelector(".fa-cart-shopping");
 
-botaoCarrinho.addEventListener("click", ()=>{
+botaoCarrinho.addEventListener("click", () => {
     window.location.href = "carrinho.html"
 })
 
-function adicionarCarrinho (nome, preco){
-
-    // utilizando um dicionario, para guardar os valores (nome, preco) e utilizando do metodo do JASON.stringify 
-    // para transformar em String e guardando na variavel nome para salvar em objetos diferentes
-    let lista = {"nomeProduto": nome, "precoProduto": preco}
-    localStorage.setItem(nome, JSON.stringify(lista))
+// Inicializa todos os produtos a disposição simulando um banco de dados local
+function inicializarProdutos() {
+    if (localStorage.length == 0) {
+        for (let i = 0; i < LISTA_PRODUTOS.length; i++) {
+            localStorage.setItem(LISTA_PRODUTOS[i].nomeProduto, JSON.stringify(LISTA_PRODUTOS[i]))
+        }
+    }
 }
 
+// funcao que adiciona quantidade a um produto a partir de um input, tirando-o de texto para acessar a chave do dicionario e depois retransformo para adicionar 
+// a nova linha
+function adcionarQuantidadeProduto(nomeProduto) {
+    let inputQuantidade = document.getElementById(nomeProduto)
 
-// function verifica
+    let valor = localStorage.getItem(nomeProduto)
+    let listaOriginal = JSON.parse(valor)
+
+    listaOriginal["quantidade"] = inputQuantidade.value
+
+    localStorage.setItem(listaOriginal["nomeProduto"], JSON.stringify(listaOriginal))
+}
+
+function filtrarProdutos(){
+    let inputRadioFiltro = document.getElementsByName("filtros")
+
+    for (let i = 0; i < inputRadioFiltro.length; i++) {
+        if(inputRadioFiltro[i].checked == true){
+            let categoria = inputRadioFiltro[i].id
+        }
+    }
+
+    
+    //console.log(inputRadioFiltro)
+}
+
+let inputRadioFiltro = document.querySelectorAll(`#Alimentos`)
+inputRadioFiltro[0].addEventListener('change', filtrarProdutos())
+
+function removerFiltros(){
+    let inputRadioFiltro = document.getElementsByName("filtros")
+
+    for (let i = 0; i < inputRadioFiltro.length; i++) {
+        inputRadioFiltro[i].checked = false
+    }
+}
+
+removerFiltros()
+
+inicializarProdutos()
+
+
+//pegarFiltro()
